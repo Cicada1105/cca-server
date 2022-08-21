f64edcc3374e2b1be268204afe0ed9ba0f4b9d51
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro.local>
2022-08-21 11:51:38 -0400

    temp commit

--- email/index.js
+++ email/index.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const nodemailer = require('nodemailer');
+const { google } = require("googleapis");
+const { retrieveClientInfo, updateClientInfo } = require("./utils.js");
 
 function Send(req,res) {
 	let response;
@@ -28,24 +31,83 @@ function Send(req,res) {
 	});
 }
 function sendMail(userData) {
-	let transporter = nodemailer.createTransport({
+	const createTransport = async () => {
+		const CLIENT_ID = retrieveClientInfo("client_id");
+		const CLIENT_SECRET = retrieveClientInfo("client_secret");
+		const REFRESH_TOKEN = retrieveClientInfo("refresh_token");
+		const ACCESS_TOKEN = retrieveClientInfo("access_token");
+
+		const oauth2Client = new google.auth.OAuth2(
+			CLIENT_ID,
+			CLIENT_SECRET,
+			'https://developers.google.com/oauthplayground'
+		);
+		oauth2Client.setCredentials({
+			refresh_token: tokens.refresh_token
+		});
+		// Listen for new token events to have the latest, most updated tokens
+		/*oauth2Client.on("token",(tokens) => {
+			if (tokens.refresh_token) {
+				oauth2Client.setCredentials({
+					refresh_token: tokens.refresh_token
+				});
+			}
+			else { // Received an access token
+				updateClientInfo("access_token",tokens.access_token);
+			}
+		})*/
+
+		const accessToken = await new Promise((resolve,reject) => {
+			oauth2Client.getToken((err, token) => {
+				err && reject(err);
+				resolve(token);
+			})
+		})
+		console.log(`accessToken = ${accessToken}`);
+		const transporter = nodemailer.createTransport({
+			host: "smtp.google.com",
+			service: 'gmail',
+			port: 465,
+			secure:true,
+			auth: {
+				type:"OAuth2",
+				user:'carlcolvinarts@gmail.com',
+				clientId: CLIENT_ID,
+				clientSecret: CLIENT_SECRET,
+				refreshToken: REFRESH_TOKEN,
+				accessToken
+			}
+		});
+		transporter.verify((err, success) => {
+			if (err)
+				console.log(err);
+			else 
+				console.log("transporter ready to take messages");
+		})
+
+		return transporter;
+	}
+	/*const transporter = nodemailer.createTransport({
 		host: "smtp.google.com",
 		service: 'gmail',
+		port: 465,
 		auth: {
 			user:'carlcolvinarts@gmail.com',
-			pass:'Sequestered_Sylvester92'
+			pass:'Sequestered_Invester92'
 		}
-	});
-
-	let mailerOptions = {
+	});*/
+	const mailerOptions = {
 		from: userData.email,
 		to: 'joshicolvin@gmail.com',
 		subject: "CarlColvinArts Website Message",
-		text: `Subject: ${userData.subject}\nName: ${userData.name}\nEmail: ${userData.email}\nMessage: ${userData.message}\n ` 
+		html: `
+			<h3 style="display:inline;">Subject:</h3> <p style="display:inline;font-size:0.9rem;">${userData.subject}</p><br />
+			<h3 style="display:inline;">Name:</h3> <p style="display:inline;font-size:0.9rem;">${userData.name}</p><br />
+			<h3 style="display:inline;">Email:</h3> <p style="display:inline;font-size:0.9rem;">${userData.email}</p><br />
+			<h3 style="display:inline;">Message:</h3> <p style="display:inline;font-size:0.9rem;">${userData.message}</p><br /> ` 
 	};
-
-	let promise = new Promise(function(resolve, reject) {
-		transporter.sendMail(mailerOptions, function(err, info) {
+	const promise = new Promise(function(resolve, reject) {
+		createTransport().sendMail(mailerOptions, function(err, info) {
 			if (err) 
 				reject(err)
 			else 
