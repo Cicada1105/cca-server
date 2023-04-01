8ce8af26f17586c9df328ced4c723a023e84c59a
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2023-04-01 18:00:53 -0400

    Extrapolated database connection functionality by creating a function to receive a collection and return apending collection result along with a cleanup function for closing the database connection

--- utils/mongodb.js
+++ utils/mongodb.js
@@ -17,6 +17,24 @@ const mongoConnectionOptions = {
 
 const mongoClient = new MongoClient(mongoURI, mongoConnectionOptions);
 
+async function getDatabaseCollection(collectionName) {
+	try {
+		await mongoClient.connect();
+
+		let db = await mongoClient.db('cca_database');
+
+		return {
+			collection: db.collection(collectionName),
+			closeConnection: () => {
+				mongoClient.close()
+			}
+		}
+	} catch(e) {
+		reject("Error connecting to the database");
+		console.log(e);
+	}
+}
+
 module.exports = { 
-	mongoClient, ObjectId
+	getDatabaseCollection, ObjectId
 }
