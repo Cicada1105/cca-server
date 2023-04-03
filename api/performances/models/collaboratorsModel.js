3ace9ec7771f5c1b5119a44aa990206c401a7604
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2023-04-03 14:13:07 -0400

    Connected collaborators public api to Mongo

--- api/performances/models/collaboratorsModel.js
+++ api/performances/models/collaboratorsModel.js
@@ -3,21 +3,26 @@
 */
 
 const { getFileData } = require("../../utils.js");
+const { getDatabaseCollection } = require('../../../utils/mongodb.js');
 
 function getAllCollaborators() {
 	// Return promise that resolves with collaborators data
 	return new Promise((resolve,reject) => {
-		// Remove unnecessary id from data to be returned to front end
-		let collaborators = getFileData("./site_data/collaborators.json");
+		getDatabaseCollection('collaborators').then(async ({ collection, closeConnection }) => {
+			let collaborators = await collection.find({}).toArray();
 
-		let updatedCollaborators = [];
-		collaborators.forEach(collaborator => {
-			// Extract out current id from rest of info
-			let { id, ...rest } = collaborator
-			// Store rest of collaborator info without id
-			updatedCollaborators.push(rest);
-		})
-		resolve(updatedCollaborators);
+			// Now that the collection has been queried, close the database connection
+			closeConnection();
+			
+			let updatedCollaborators = [];
+			collaborators.forEach(collaborator => {
+				// Extract out current id from rest of info
+				let { _id, ...rest } = collaborator
+				// Store rest of collaborator info without id
+				updatedCollaborators.push(rest);
+			})
+			resolve(updatedCollaborators);
+		});
 	})
 }
 
