496963cd982b2e64bd050bfebeab2323a4d8e7b6
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2023-04-04 12:47:44 -0400

    Connected reedmaking public api to Mongo

--- api/models/reedmakingModel.js
+++ api/models/reedmakingModel.js
@@ -3,20 +3,23 @@
 */
 
 const { getFileData } = require("../utils.js");
+const { getDatabaseCollection } = require('../../utils/mongodb.js');
 
 function getAllPricings() {
 	// Return promise that resolves to fill data
 	return new Promise((res,rej) => {
-		let pricings = getFileData('./site_data/reedmaking.json');
-		// Remove unnecessary id from data to be returned to front end
-		let updatedPricings = [];
-		pricings.forEach(pricing => {
-			// Extract out current id from rest of info
-			let { id, ...rest } = pricing;
-			// Store rest of pricing info without id
-			updatedPricings.push(rest);
-		})
-		res(updatedPricings);
+		getDatabaseCollection('reedmaking').then(async ({ collection, closeConnection }) => {
+			const pricings = await collection.find({}).toArray();
+			// Remove unnecessary id from data to be returned to front end
+			let updatedPricings = [];
+			pricings.forEach(pricing => {
+				// Extract out current id from rest of info
+				let { id, ...rest } = pricing;
+				// Store rest of pricing info without id
+				updatedPricings.push(rest);
+			})
+			res(updatedPricings);
+		});
 	});
 }
 
