b2ea50bf94ed83963bbb5fad06dbe06d024b836b
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2023-04-11 13:56:25 -0400

    Connected music stand crud functionality to the Mongo database instead of a file

--- admin/api/performances/present/models/currentMusicModel.js
+++ admin/api/performances/present/models/currentMusicModel.js
@@ -2,43 +2,38 @@
 	File contains request methods for dealing directly with the corresponding data
 */
 
-// Import performance data to be used by the model
-const performancesPath = "./site_data/performance.json";
-// Require method to retrieve file data
-const { getFileData } = require("../../../../utils.js");
-// Require method for writing to file
-const { writeToFile } = require("../../../utils.js");
-// Import uuid for adding new resource
-const { v4: uuidv4 } = require("uuid");
+const { getDatabaseCollection, ObjectId } = require('../../../../../utils/mongodb.js');
 
+const PERFORMANCES_ID = '64359642dd85c7fd598530ca';
 /*
 	Future add documentation
 */
 function add(newSong) {
 	return new Promise((resolve,reject) => {
-		// Get performances from file
-		let parsedPerformances = getFileData(performancesPath);
+		getDatabaseCollection('performances').then(async ({ collection, closeConnection }) => {
+			let result = await collection.findOneAndUpdate({
+				_id: new ObjectId(PERFORMANCES_ID)
+			}, {
+				$push: {
+					present: {
+						id: new ObjectId(),
+						name: newSong['name'],
+						by: newSong['by'],
+						description: newSong['description']
+					}
+				}
+			});
 
-		// Pull out present music
-		let currentMusic = parsedPerformances["present"];
-		// Add unique id to new song
-		let newSongWithID = {
-			id:uuidv4(),
-			...newSong
-		}
-		// Push new song into present performances array
-		currentMusic.push(newSongWithID);
-		// Update performances data to reflect resent present performances change
-		parsedPerformances["present"] = currentMusic;
+			// Close connection now that database operations are done
+			closeConnection();
 
-		// Write data to file, catching any error that may occur
-		try {
-			writeToFile(performancesPath,JSON.stringify(parsedPerformances));
-			resolve("Successfully added new present performance");
-		} catch(e) {
-			console.log(err);
-			reject("Internal Server Error. Try again later");
-		}
+			if (result.ok) {
+				resolve(`Successfully added new song: ${newSong['name']}`);
+			}
+			else {
+				reject("Internal Server Error. Try again later");
+			}
+		});
 	})
 }
 /*
@@ -46,29 +41,29 @@ function add(newSong) {
 */
 function update(editedSong) {
 	return new Promise((resolve,reject) => {
-		// Get performances from file
-		let parsedPerformances = getFileData(performancesPath);
+		getDatabaseCollection('performances').then(async ({ collection, closeConnection }) => {
+			let result = await collection.findOneAndUpdate({
+				_id: new ObjectId(PERFORMANCES_ID)
+			}, {
+				$set: {
+					'present.$[el].name': editedSong['name'],
+					'present.$[el].by': editedSong['by'],
+					'present.$[el].description': editedSong['description']
+				}
+			}, {
+				arrayFilters: [{ 'el.id': new ObjectId(editedSong['id']) }]
+			});
 
-		// Pull out present music
-		let currentMusic = parsedPerformances["present"];
+			// Close connection now that database operations are done
+			closeConnection();
 
-		// Find index in storage of song that matches the id of the edited song
-		let index = currentMusic.findIndex(song => song.id === editedSong.id);
-		if (index === -1)
-			reject(`Unable to find ${editedSong.name} by ${editedSong.by}`)
-		else {
-			// Update stored music
-			Object.assign(currentMusic[index],editedSong);
-			// Update present music with rest of performances
-			let updatedPerformances = { ...parsedPerformances, "present": currentMusic };
-			// Update file, reflecting changes to current music
-			try {
-				writeToFile(performancesPath,JSON.stringify(parsedPerformances));
-				resolve(`Successfully updated ${currentMusic[index].name} by ${currentMusic[index].by}`);
-			} catch(e) {
+			if (result.ok) {
+				resolve(`Successfully updated ${editedSong['name']} by ${editedSong['by']}`);
+			}
+			else {
 				reject("Internal Server Error. Try again later");
 			}
-		}
+		});
 	})
 }
 /*
@@ -76,26 +71,27 @@ function update(editedSong) {
 */
 function remove(songID) {
 	return new Promise((resolve,reject) => {
-		// Get performances from file
-		const performances = getFileData(performancesPath);
-		
-		let index = performances["present"].findIndex((performance) => performance.id === songID);
-		if (index === -1)
-			reject(`Unable to find present performance with id of: ${songID}`);
-		else {
-			// Filter out present performance who's ID matches that of songID
-			let updatedPresentPerformances = performances["present"].filter((performance) => performance.id !== songID );
-			// Update present performances with rest of data
-			let updatedPerformances = {...performances,"present":updatedPresentPerformances};
-			// Update file, reflectting new performances
-			try {
-				writeToFile(performancesPath,JSON.stringify(updatedPerformances));
+		getDatabaseCollection('performances').then(async ({ collection, closeConnection }) => {
+			let result = await collection.findOneAndUpdate({
+				_id: new ObjectId(PERFORMANCES_ID)
+			}, {
+				$pull: {
+					present: {
+						id: new ObjectId(songID)
+					}
+				}
+			});
+
+			// Close connection now that database operations are done
+			closeConnection();
+
+			if (result.ok) {
 				resolve("Successfully removed present performance!");
-			} catch(e) {
-				console.log(err);
+			}
+			else {
 				reject("Internal Server Error. Try again later");
 			}
-		}
+		});
 	})
 }
 
