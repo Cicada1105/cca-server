bccb8dca4cb43100b571348b9e899c4ea1de26a0
Josh Colvin <joshuacolvin@Joshuas-MacBook-Pro-2.local>
2024-07-22 21:08:00 -0400

    Fixed response headers to only allow from frontend or server url depending on the request.

--- index.js
+++ index.js
@@ -13,26 +13,38 @@ const server = http.createServer((req,res) => {
 	// First 3 setHeader's are for development ONLY
 	// Source: https://stackoverflow.com/questions/56339978/how-can-i-make-my-front-end-app-access-my-node-server-apis
 
+	const validFrontEndDomains = [ 'http://carlcolvinarts.com', 'https://carlcolvinarts.com', 'www.carlcolvinarts.com', 'carlcolvinarts', 'www.carlcolvinarts' ];
+	const defaultFrontEndDomain = 'https://carlcolvinarts.com';
+	const validServerDomains = [ 'https://cca-server-41u0.onrender.com' ];
+	const defaultServerDomain = 'https://cca-server-41u0.onrender.com';
+
 	if (req.url !== "favicon.ico") {
 		if (req.url.startsWith("/api")) {
-    	res.setHeader("Access-Control-Allow-Origin", "carlcolvinarts.com");
+    	res.setHeader("Access-Control-Allow-Origin", validFrontEndDomains.find((val) => val === req['headers']['origin']) || defaultFrontEndDomain);
     	res.setHeader("Access-Control-Allow-Methods", "GET");
     	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
 			API.Router(req,res);
 		}
-		else {
-    	res.setHeader("Access-Control-Allow-Origin", "https://cca-server-41u0.onrender.com");
+		else if ((req.url === "/contact") && (req.method === "POST")) {
+    	res.setHeader("Access-Control-Allow-Origin", validFrontEndDomains.find((val) => val === req['headers']['origin']) || defaultFrontEndDomain);
+    	res.setHeader("Access-Control-Allow-Methods", "POST");
+    	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+			EMAIL.Send(req,res);
+		}
+		else if (req.url.startsWith("/cca-admin")) {
+    	res.setHeader("Access-Control-Allow-Origin", validServerDomains.find((val) => val === req['headers']['origin']) || defaultServerDomain);
     	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Content-Length");
     	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    	
-			if ((req.url === "/contact") && (req.method === "POST"))
-				EMAIL.Send(req,res);
-			else if (req.url.startsWith("/cca-admin"))
-				ADMIN.Router(req,res);
-			else {
-				res.writeHead(301,{"Location":`${SERVER_URL}/cca-admin-login`});
-				res.end();
-			}
+console.log(res.getHeaders());
+			ADMIN.Router(req,res);
+		}
+		else {
+			res.writeHead(404,{
+				"Content-Type":"text/strings"
+			});
+			res.end("Unable to find resource");
 		}
 	}
 });
